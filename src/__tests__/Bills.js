94e5f3991726bedea59da4dd0df45222cca448d0
Christine <50479077+christinebogdan@users.noreply.github.com>
2021-05-05 12:49:24 +0200

    tests for container/Bills and views/Bills

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -4,13 +4,87 @@ import Bills from "../containers/Bills.js";
 import { bills } from "../fixtures/bills.js";
 import { localStorageMock } from "../__mocks__/localStorage";
 
+// ADDED BY ME
+
+import LoadingPage from "../views/LoadingPage.js";
+import ErrorPage from "../views/ErrorPage.js";
+import userEvent from "@testing-library/user-event";
+import firestore from "../app/Firestore.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes";
+
+const bill = [
+  {
+    id: "47qAXb6fIm2zOKkLzMro",
+    vat: "80",
+    fileUrl:
+      "https://firebasestorage.googleapis.com/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a",
+    status: "pending",
+    type: "Hôtel et logement",
+    commentary: "séminaire billed",
+    name: "encore",
+    fileName: "preview-facture-free-201801-pdf-1.jpg",
+    date: "2004-04-04",
+    amount: 400,
+    commentAdmin: "ok",
+    email: "a@a",
+    pct: 20,
+    email: "john.snow@billed.com",
+  },
+];
+
 describe("Given I am connected as an employee", () => {
-  describe("When I am on Bills Page", () => {
-    test("Then bill icon in vertical layout should be highlighted", () => {
-      const html = BillsUI({ data: [] });
-      document.body.innerHTML = html;
-      //to-do write expect expression
+  // TEST WRITTEN BY ME
+  describe("When BillsUI is called", () => {
+    // CHANGE TEST DESCRIPTION TO TEST RESULT - DONE
+    test("Then Loading Page should be rendered when loading is true", () => {
+      const referenceLoadingPage = LoadingPage();
+      const result = BillsUI({ data: bills, loading: true });
+      // const loadingPage = jest.fn(() => {
+      //   return LoadingPage();
+      // });
+      // why here toEqual not toHaveBeenCalled
+      // expect(result).toEqual(loadingPage());
+      expect(result).toEqual(referenceLoadingPage);
     });
+
+    test("Then ErrorPage should be rendered when loading is false and error is true", () => {
+      const error = "Error";
+      const referenceErrorPage = ErrorPage(error);
+      const result = BillsUI({ data: bills, loading: false, error });
+      expect(result).toEqual(referenceErrorPage);
+    });
+  });
+
+  describe("When I am on Bills Page", () => {
+    let onNavigate;
+    let billsClass;
+    beforeEach(() => {
+      onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
+
+      // billsClass not needed by ALL
+      billsClass = new Bills({
+        document,
+        onNavigate,
+        firestore,
+        localStorage,
+      });
+    });
+
+    test("Then bill icon in vertical layout should be highlighted", () => {
+      // need storage here for VerticalLaout in BillsUI
+      const html = BillsUI({ data: bills });
+      document.body.innerHTML = html;
+      //to-do write expect expression DONE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+      const billIcon = screen.getByTestId("icon-window");
+      expect(billIcon.classList.contains("active-icon")).toBeTruthy;
+    });
+
     test("Then bills should be ordered from earliest to latest", () => {
       const html = BillsUI({ data: bills });
       document.body.innerHTML = html;
@@ -22,6 +96,58 @@ describe("Given I am connected as an employee", () => {
       const antiChrono = (a, b) => (a < b ? 1 : -1);
       const datesSorted = [...dates].sort(antiChrono);
       expect(dates).toEqual(datesSorted);
+    });
+
+    // can I combine this test with the next one? Can I somehow reset the function call?
+    // check if fails if local Storage is omitted
+    test("Then event handling should be set up for the new bill button", () => {
+      document.body.innerHTML = BillsUI({ data: bills });
+      const newBillButton = screen.getByTestId("btn-new-bill");
+      const handleClickNewBill = jest.fn(billsClass.handleClickNewBill);
+      newBillButton.addEventListener("click", handleClickNewBill);
+      userEvent.click(newBillButton);
+      expect(handleClickNewBill).toHaveBeenCalled();
+    });
+
+    // test Dashboard.js als Vorlage
+    test("Then all event handling should be set up for the eye icons", () => {
+      const html = BillsUI({ data: bill });
+      document.body.innerHTML = html;
+      const handleClickIconEye = jest.fn(billsClass.handleClickIconEye);
+      const eye = screen.getByTestId("icon-eye");
+      eye.addEventListener("click", (e) => handleClickIconEye(eye));
+      userEvent.click(eye);
+      expect(handleClickIconEye).toHaveBeenCalled();
+    });
+
+    describe("When I click on the New Bill button", () => {
+      test("Then it should render the New Bill form", () => {
+        const data = [];
+        const loading = false;
+        const error = null;
+        const pathname = ROUTES_PATH["NewBill"];
+        const html = ROUTES({ pathname, data, loading, error });
+        document.body.innerHTML = html;
+        expect(screen.getAllByText("Send a fee")).toBeTruthy();
+      });
+    });
+
+    // moved this inside the previous describe to get the beforeEach setup
+    describe("When I click on the eye icon of a bill", () => {
+      test("Then it should show the bill in a modal", () => {
+        const html = BillsUI({ data: bill });
+        document.body.innerHTML = html;
+        // tests Dahsboard.js als Vorlage
+        const handleClickIconEye = jest.fn(billsClass.handleClickIconEye);
+        const eye = screen.getByTestId("icon-eye");
+        eye.addEventListener("click", (e) => handleClickIconEye(eye));
+        userEvent.click(eye);
+        expect(handleClickIconEye).toHaveBeenCalled();
+        const modal = screen.getAllByText("Fee");
+        // did not work with getByRole("dialog")
+        // can I do this with getAllByText?
+        expect(modal).toBeTruthy();
+      });
     });
   });
 });
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,14 +1,41 @@
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
-
+import { fireEvent, screen } from "@testing-library/dom";
+import NewBillUI from "../views/NewBillUI.js";
+import NewBill from "../containers/NewBill.js";
+import firestore from "../app/Firestore.js";
+import userEvent from "@testing-library/user-event";
+import { localStorageMock } from "../__mocks__/localStorage";
+import { ROUTES, ROUTES_PATH } from "../constants/routes";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
-  })
-})
+    test("Then all event handling should be set up for the new Bill Form ", () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+
+      // passes when I comment out firestore
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        firestore,
+        localStorage,
+      });
+
+      const formNewBill = screen.getByTestId("form-new-bill");
+      const handleSubmit = jest.fn(newBill.handleSubmit);
+      // fails when (e) => handleSubmit
+      // fails when NewBill.js lines 62 and 63 not commented out
+      formNewBill.addEventListener("submit", handleSubmit);
+      fireEvent.submit(formNewBill);
+      expect(handleSubmit).toHaveBeenCalled();
+    });
+  });
+});

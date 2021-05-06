d689bfee1344c6a623e83c1b1963b15f5abd7e9f
Christine <50479077+christinebogdan@users.noreply.github.com>
2021-05-06 14:27:14 +0200

    beforeEach for NewBill tests, first test for added event handling

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -8,8 +8,10 @@ import { ROUTES, ROUTES_PATH } from "../constants/routes";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then all event handling should be set up for the new Bill Form ", () => {
-      const html = NewBillUI();
+    let html;
+    let onNavigate;
+    beforeEach(() => {
+      html = NewBillUI();
       document.body.innerHTML = html;
 
       Object.defineProperty(window, "localStorage", {
@@ -17,15 +19,16 @@ describe("Given I am connected as an employee", () => {
       });
       window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
 
-      const onNavigate = (pathname) => {
+      onNavigate = (pathname) => {
         document.body.innerHTML = ROUTES({ pathname });
       };
-
+    });
+    test("Then submit button in new Bill Form should have Event Handler added", () => {
       // passes when I comment out firestore
       const newBill = new NewBill({
         document,
         onNavigate,
-        firestore,
+        // firestore,
         localStorage,
       });
 
@@ -37,5 +40,78 @@ describe("Given I am connected as an employee", () => {
       fireEvent.submit(formNewBill);
       expect(handleSubmit).toHaveBeenCalled();
     });
+
+    test("Then file input field should have Event Handler added and only accept types", () => {
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        firestore,
+        localStorage,
+      });
+
+      const fileInput = screen.getByTestId("file");
+      // why does this fail if I remove [] around new File
+      const file = [new File(["hello"], "hello.png", { type: "image/png" })];
+      const handleChangeFile = jest.fn(newBill.handleChangeFile);
+      // does this not add a second event Listener to element?
+      fileInput.addEventListener("change", handleChangeFile);
+      // https://testing-library.com/docs/ecosystem-user-event/#uploadelement-file--clickinit-changeinit-
+      userEvent.upload(fileInput, file);
+      expect(handleChangeFile).toHaveBeenCalled();
+    });
+
+    test("then file input should only accept types png, jpg and jpeg", () => {
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        firestore,
+        localStorage,
+      });
+
+      const fileInput = screen.getByTestId("file");
+      const handleChangeFile = jest.fn(newBill.handleChangeFile);
+      fileInput.addEventListener("change", handleChangeFile);
+      const png = new File(["hello"], "hello.png", { type: "image/png" });
+      userEvent.upload(fileInput, png);
+      const expectation = "";
+      expect(fileInput.value).not.toEqual(expectation);
+
+      const jpg = new File(["hello"], "hello.png", { type: "image/jpg" });
+      userEvent.upload(fileInput, jpg);
+      const jpeg = new File(["hello"], "hello.png", { type: "image/jpeg" });
+      userEvent.upload(fileInput, jpeg);
+      const gif = new File(["hello"], "hello.png", { type: "image/gif" });
+      userEvent.upload(fileInput, gif);
+    });
+  });
+});
+
+// test d'intégration GET
+describe("Given I am a user connected as Employee", () => {
+  describe("When I post a New Bill from the New Bill Form", () => {
+    test("posts new bill to firestore bills collection", async () => {
+      const getSpy = jest.spyOn(firebase, "get");
+      const bills = await firebase.get();
+      expect(getSpy).toHaveBeenCalledTimes(1);
+      expect(bills.data.length).toBe(4);
+    });
+    // test("fetches bills from an API and fails with 404 message error", async () => {
+    //   firebase.get.mockImplementationOnce(() =>
+    //     Promise.reject(new Error("Erreur 404"))
+    //   );
+    //   const html = DashboardUI({ error: "Erreur 404" });
+    //   document.body.innerHTML = html;
+    //   const message = await screen.getByText(/Erreur 404/);
+    //   expect(message).toBeTruthy();
+    // });
+    // test("fetches messages from an API and fails with 500 message error", async () => {
+    //   firebase.get.mockImplementationOnce(() =>
+    //     Promise.reject(new Error("Erreur 500"))
+    //   );
+    //   const html = DashboardUI({ error: "Erreur 500" });
+    //   document.body.innerHTML = html;
+    //   const message = await screen.getByText(/Erreur 500/);
+    //   expect(message).toBeTruthy();
+    // });
   });
 });
